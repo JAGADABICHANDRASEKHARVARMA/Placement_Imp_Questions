f7243e11e61b06e62410587a7709ba0842b64d8e
JAGADABI CHANDRASEKHAR VARMA <68846646+JAGADABICHANDRASEKHARVARMA@users.noreply.github.com>
2021-08-03 23:21:20 +0530

    Update CountDigits.java

--- DSA/Mathematics/CountDigits.java
+++ DSA/Mathematics/CountDigits.java
@@ -16,3 +16,24 @@ class chandu{
         }
     }
 
+
+
+//Iterative Solution
+// You are using Java
+import java.util.*;
+class chandu{
+    static int countDigits(long n){
+        int count = 0;
+        while(n!=0){
+            n = n/10;
+            ++count;
+        }
+        return count;
+        
+        }    
+        public static void main(String[] args){
+            long n = 193873;
+            System.out.println(countDigits(n));
+        }
+    }
+
